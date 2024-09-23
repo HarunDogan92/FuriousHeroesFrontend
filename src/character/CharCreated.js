837faf5d47bd171ce3025da1349886bbf30d0d80
harun <harun.dogan@stud.fh-campuswien.ac.at>
2024-09-23 16:45:44 +0200

    goToBase

--- src/character/CharCreated.js
+++ src/character/CharCreated.js
@@ -6,18 +6,22 @@ const CharCreated = () => {
   const { avatar } = state;
   const navigate = useNavigate()
 
+  const goToBase = () => {
+    navigate('/base')
+  }
+
   return (
     <div className="mainContainer">
       <div className={'titleContainer'}>
         <div>Furious Heroes</div>
       </div>
       <div><img src={avatar} /></div>
-      <div>Start your new Adventure!</div>
+      <div>This is your Character!</div>
       <div className={'buttonContainer'}>
         <input
           className={'inputButton'}
           type="button"
-          onClick={navigate('/base')}
+          onClick={goToBase}
           value= 'Go to Base'
         />
       </div>
